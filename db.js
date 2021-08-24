37b2bc521b7ce4227b07147a5db978fa1d1e1dfc
prhodesy <peter.rhodes@gmail.com>
2021-08-24 20:09:55 +0700

    merged db.js logic into server.js

--- db.js
+++
@@ -1,4 +0,0 @@
-var fs = require('fs');
-var db = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
-
-module.exports = db;
--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 var args = process.argv.slice(2);
 
-const dbJsData = module.require('./db.js');
-const dbJsonData = './db.json';
+const dbJsonFile = 'db.json';
 var routerData;
-if (args.length === 1 && args[0] === 'db.json') {
-  routerData = dbJsonData;
+if (args.length === 1 && args[0] === dbJsonFile) {
+  routerData = dbJsonFile;
   console.log('Non-safe HTTP methods will modify the db.json file');
 } else {
-  routerData = dbJsData;
+  const fs = require('fs');
+  routerData = JSON.parse(fs.readFileSync(dbJsonFile, 'utf8'));
   console.log('The db.json file will not be modified, any changes will be lost when the server is shut down');
 }
 
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
-
-//var jsonData = module.require("./db.js")
 const router = jsonServer.router(routerData);
-
 const portNumber = 3000;
 
 server.use(middlewares);
